bacfafd3b76bc995f6af5b47aff8459e11526f4f
josella magliotti <josellamagliotti@gmail.com>
2022-11-17 21:46:16 -0300

    falta limpiar los campos de seleccion y listo

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let horario
 let nombre
 let email
 let apellido
+let turnos=[]
 
 
 // fecha actual con uso de DATE
@@ -49,7 +50,7 @@ function controlDia(){
         //remuevo la clase si es que existe 
         seleccionadoAnterior.className=""
     }
-     
+        
      if(parseInt(diaSeleccionado.innerHTML )>=currentDay ){ // si no existe entonces le doy la clase diaseleccionado 
         diaSeleccionado.className="diaseleccionado"
         //valor a la variable global
@@ -156,7 +157,6 @@ botonreserva.addEventListener("click", (e) => {
     e.preventDefault()
 
     nombre=(document.getElementById("nombreCliente").value).toLowerCase()
-    email=document.getElementById("email").value
     apellido=(document.getElementById("apellidoCliente").value).toLowerCase()
 
 
@@ -164,6 +164,8 @@ botonreserva.addEventListener("click", (e) => {
 
         const response = await  fetch('https://jsonplaceholder.typicode.com/users')
         const clientes = await response.json()
+      
+        
     
         yaTieneReserva(clientes, nombre)
     
@@ -171,6 +173,19 @@ botonreserva.addEventListener("click", (e) => {
       }
     
       obtenerDatos()
+
+
+      //objeto Turno con todos los datos obtenidos previamente
+      turnos.push({
+        clienteNombre:nombre,
+        clienteApellido:apellido,
+        horaturno:horario,
+        diaturno:dia,
+      }
+      )
+      
+      localStorage.setItem("turnos", JSON.stringify(turnos))  // GUARDADO LOCAL DE LOS DATOS DE RESERVA
+
     
 }
 )
@@ -182,14 +197,16 @@ botonreserva.addEventListener("click", (e) => {
 
   function yaTieneReserva(clientes, nombre) {
 
-    const existe= clientes.some(elemento => elemento === nombre);
+   
+    const existeNombre= clientes.some(elemento => elemento === nombre);
+    const existeApelido= clientes.some(elemento => elemento === apellido);
+    
 
     
-      if((!existe)||(nombre !== null))
+      if((!existeNombre)&&(nombre.trim() !== "")&&(!existeApelido)&&(apellido.trim() !== ""))
       { Swal.fire({
         title: "TURNO RESERVADO",
-        /*text: (" dia :" + dia+ "/"+ (monthNumber+1)+"/"+(currentYear)+ " a la hora"+horario),*/
-        text: (" Gracias " + nombre + " te enviaremos un mail con los datos del turno al " +email),
+        text: (" Gracias " + nombre + " la fecha reservada es :" + dia+ "/"+ (monthNumber+1)+"/"+(currentYear)+ " en el horario(hs) "+horario),
         icon: 'success',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
@@ -204,13 +221,52 @@ botonreserva.addEventListener("click", (e) => {
           )
         }
         })
-                 
-      } else {
-        alert("nombre esta vacio")
-                 
-               
-                
+   
+   
+      } else if((existeNombre)&&(nombre.trim() !== "")&&(existeApelido)&&(apellido.trim() !== "")){
+
+        Swal.fire({
+          title: 'ya tienes un turno reservado. Deseas cambiarlo?',
+          showDenyButton: true,
+          showCancelButton: true,
+          confirmButtonText: 'Si',
+          denyButtonText: `No`,
+        }).then((result) => {
+          /* Read more about isConfirmed, isDenied below */
+          if (result.isConfirmed) {
+
+            Swal.fire({
+              title: "TURNO RESERVADO",
+              text: (" Gracias " + nombre + " la fecha reservada es :" + dia+ "/"+ (monthNumber+1)+"/"+(currentYear)+ " en el horario(hs) "+horario),
+              icon: 'success',
+              showCancelButton: true,
+              confirmButtonColor: '#3085d6',
+              cancelButtonColor: '#d33',
+              confirmButtonText: 'confirmar'
+              }).then((result) => {
+              if (result.isConfirmed) {
+                Swal.fire(
+                  'Reservado',
+                  'Cualquier consulta estamos a disposicion',
+                  'success'
+                )
+              }
+              })
+            
+          } else if (result.isDenied) {
+            Swal.fire('Se mantiene el turno ya reservado! Cualquier consulta quedamos a disposicion. Gracias por contactarnos')
+          }
+        })
+
+      } else{
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Faltan completar datos para la reserva',
+          
+        })
       }
-     } 
-     
+  }
+
+ 
 
